22044c7bf8cf48cb3b3eed93afefc1cfd1a02f1f
Matt Grogan <mattgrogan.dev@gmail.com>
2022-03-16 19:36:22 -0700

    switched things up and added an api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,72 @@
+import { useEffect, useState } from "react";
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+
+const fetchPost = async () =>
+  fetch("https://random-data-api.com/api/coffee/random_coffee?size=5");
 
 function App() {
-  const name = "Matt";
-  const x = true;
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: `Psychiatrist Appointment`,
+      day: "Mar 16th at 1:15 PM",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: `Code Assessment`,
+      day: "Mar 17th at 11:00 AM",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: `Code Review`,
+      day: "Mar 17th at 11:00 AM",
+      reminder: true,
+    },
+  ]);
+
+  const [coffees, setCoffees] = useState([]);
+
+  useEffect(() => {
+    fetchPost().then((res) => {
+      console.log(res);
+      res.json().then((data) => {
+        setCoffees(data);
+      });
+    });
+  }, []);
+
+  console.log(coffees);
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
+  // Add Task
+  const addTask = (task) => {
+    setTasks((prv) => [...prv, task]);
+  };
 
   return (
     <div className="container">
-      <Header />
+      <Header onAdd={addTask} />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No Tasks To Show"
+      )}
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,33 @@
-import PropTypes from 'prop-types';
-import Button from './Button'
+import PropTypes from "prop-types";
+import Button from "./Button";
 
-const Header = ({ title }) => {
-
-  const onClick = () => {
-      console.log('Click')
-  }
-
+const Header = ({ title, onAdd }) => {
   return (
-      <header className='header'>
-          <h1>{title}</h1>
-          <Button color='green' text='Add' onClick={onClick} />
-
-        
-      </header>
-  )
+    <header className="header">
+      <h1>{title}</h1>
+      <Button
+        color="green"
+        text="Add"
+        onClick={() =>
+          onAdd({
+            id: 13,
+            text: `hello world`,
+            day: "Mar 25 at 1:15 PM",
+            reminder: true,
+          })
+        }
+      />
+    </header>
+  );
 };
 
 Header.defaultProps = {
-    title: 'Task Tracker'
-}
+  title: "Task Tracker",
+};
 
 Header.propTypes = {
-    title: PropTypes.string.isRequired,
-}
-
+  title: PropTypes.string.isRequired,
+};
 
 //CSS in JS
 // const headingStyle = {
